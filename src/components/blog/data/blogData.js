5d8e43214e04efda6fd606b3c37a0ccd62d27073
Avyaan Verma <avyaanver@gmail.com>
2024-06-15 16:47:01 +0530

    Individual Blog created
    Fetched Data via Local Database
    
    Images are to be loaded via URL

--- src/components/blog/data/blogData.js
+++ src/components/blog/data/blogData.js
@@ -2,10 +2,29 @@
 const blogData = [
     {
       id: 1,
-      title: 'AI ABC',
-      thumbnail:'./thumbnail.webp',
+      title: 'Adoption of blockchain technology in India’s financial sector',
+      thumbnail:'https://online.stanford.edu/sites/default/files/inline-images/1600X900-How-does-blockchain-work.jpg',
+      cover:"https://www.telefonica.com/en/wp-content/uploads/sites/5/2022/06/Imagen1.jpg?w=1224&h=673&crop=1",
       subtitle: 'This is the latest blog post subtitle.',
-      body: 'This is the body of the latest blog post.',
+      author: 'Lavi Perchik',
+      body: `
+      <p class="mb-2">Our everyday lives are being impacted by modern technology, from giving commands with voice notes to controlling devices with a remote. Blockchain technology is widely utilized in banking, digital asset trading, and other industries because of its decentralized, enforceable, and sharing characteristics. In fintech research, blockchain has become a hot topic that is impacting how traditional financial forms are evolving. It is considered the digital economy's cornerstone. A data structure that maintains transactional records and, by guaranteeing security, transparency, and decentralization, removes the chance of fraudulent activity or transaction replication without the involvement of a third party is known as blockchain technology.</p>
+      <h1 class="text-2xl font-bold mb-4">Impact of Blockchain Technology on Everyday Life and Financial Sector</h1>
+
+      <p class="mb-2">Blockchain technology has a great deal of potential, especially for the financial industry. In reality, because they were mostly dependent on a single expert to manage the workload, financial institutions encountered enormous difficulties during the demonetization period in handling the sheer number of transactions. The Reserve Bank of India (RBI) has pushed banks to use blockchain technology in order to address this problem. Blockchain technology offers several advantages, including the ability to save money and time, process information more quickly, and run safe operations that are impervious to cyberattacks.</p>
+
+      <p class="mb-2">In a bid to foster blockchain technology, various banks and financial institutions are exploring the potential of blockchain to streamline operations, enhance security, and improve customer experience. For instance, the SBI, ICICI Bank, and HDFC Bank have invested in blockchain solutions to offer distributed ledger technology (DLT) solutions to the financial sector and have put in place Indian Bank’s Blockchain Infrastructure Company Private Limited (IBBIC).</p>
+
+      <p class="mb-2">Blockchain technology is being used by banks to address problems with processing e-way bills, GST invoices, and Letters of Credit (LCs). To prevent fraud, confirm transactions, and maintain ledger balance, the current procedure of generating an LC is laborious and slow, requiring human participation. These problems may be resolved by issuing LCs via blockchain. Thanks to blockchain technology, even frauds like issuing two LCs on one invoice may be readily avoided.</p>
+
+      <p class="mb-2">In addition to providing a safe environment, the shift is anticipated to remove paperwork and shorten transaction processing times. The Finacle Connect platform from Infosys, which works with blockchain technology to digitize and automate trade-related finance procedures, will serve as the foundation for the system.</p>
+
+      <p class="mb-2">Blockchain technology has enormous potential benefits not only for financial organizations but also for supply chain management firms. Blockchain can decrease mistakes and delays while monitoring expenses, employment, and releases at every level of the supply chain by recording transactions on a distributed ledger. Through source tracing, the decentralized ledger may also be used to confirm the legitimacy and fair-trade status of items.</p>
+
+      <p class="mb-2">Identifying the needs of the smallest retailer based on past business numbers, geography, and customer profiling is just one of the many advantages that blockchain technology use cases offer in the offline retail industry. Another benefit is that these small retailers' demand can be combined to streamline bulk orders from a regional distributor and place the final order with the manufacturer. Therefore, by providing a safe and open method of conducting business, blockchain technology has the potential to completely transform the way we handle supply chains and perform transactions.</p>
+
+      <p class="mb-2">Blockchain technology has many advantages, but it also has few drawbacks. It still has to be developed and faces several challenges, such as those related to scalability, security, and privacy. The possible advantages of blockchain technology significantly exceed the hazards, notwithstanding these difficulties. Blockchain has the potential to completely transform India's financial industry with the correct laws and technology developments.</p>
+      `,
       date: '9 June',
       readTime:' 5 Minute Read'
     },
@@ -14,6 +33,8 @@ const blogData = [
       title: 'ML ABC',
       thumbnail:'./thumbnail.webp',
       subtitle: 'This is a recent blog post subtitle.',
+      author: 'Lavi Perchik',
+      body: 'This is the body of the latest blog post.',
       body: 'This is the body of a recent blog post.',
       date: '9 June',
       readTime:' 5 Minute Read'
@@ -23,6 +44,8 @@ const blogData = [
       title: 'BLOCKCHAIN ABC',
       thumbnail:'./thumbnail.webp',
       subtitle: 'This is another recent blog post subtitle.',
+      author: 'Lavi Perchik',
+      body: 'This is the body of the latest blog post.',
       body: 'This is the body of another recent blog post.',
       date: '9 June',
       readTime:' 5 Minute Read'
