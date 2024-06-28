dda14f3d43208514e36eaee6281b8215223671c7
Leonardo Fernandes <leonardoschoolwork@gmail.com>
2024-06-28 13:30:31 +0530

    Added past events

---
+++ src/assets/EventAssets/Bitमंथन/index.js
@@ -0,0 +1,27 @@
+import Bitमंथन1 from "./Bitमंथन-1.jpeg";
+import Bitमंथन2 from "./Bitमंथन-2.jpeg";
+import Bitमंथन3 from "./Bitमंथन-3.jpeg";
+import Bitमंथन4 from "./Bitमंथन-4.jpeg";
+import Bitमंथन5 from "./Bitमंथन-5.jpeg";
+import Bitमंथन6 from "./Bitमंथन-6.jpeg";
+import Bitमंथन7 from "./Bitमंथन-7.jpeg";
+import Bitमंथन8 from "./Bitमंथन-8.jpeg";
+import Bitमंथन9 from "./Bitमंथन-9.jpeg";
+import Bitमंथन10 from "./Bitमंथन-10.jpeg";
+import Bitमंथन11 from "./Bitमंथन-11.jpeg";
+import Bitमंथन12 from "./Bitमंथन-12.jpeg";
+
+export {
+  Bitमंथन1,
+  Bitमंथन2,
+  Bitमंथन3,
+  Bitमंथन4,
+  Bitमंथन5,
+  Bitमंथन6,
+  Bitमंथन7,
+  Bitमंथन8,
+  Bitमंथन9,
+  Bitमंथन10,
+  Bitमंथन11,
+  Bitमंथन12,
+};
---
+++ src/assets/EventAssets/Ideathon24/index.js
@@ -0,0 +1,27 @@
+import Ideathon1 from "./Ideathon-1.jpeg";
+import Ideathon2 from "./Ideathon-2.jpeg";
+import Ideathon3 from "./Ideathon-3.jpeg";
+import Ideathon4 from "./Ideathon-4.jpeg";
+import Ideathon5 from "./Ideathon-5.jpeg";
+import Ideathon6 from "./Ideathon-6.jpeg";
+import Ideathon7 from "./Ideathon-7.jpeg";
+import Ideathon8 from "./Ideathon-8.jpeg";
+import Ideathon9 from "./Ideathon-9.jpeg";
+import Ideathon10 from "./Ideathon-10.jpeg";
+import Ideathon11 from "./Ideathon-11.jpeg";
+import Ideathon12 from "./Ideathon-12.jpeg";
+
+export {
+  Ideathon1,
+  Ideathon2,
+  Ideathon3,
+  Ideathon4,
+  Ideathon5,
+  Ideathon6,
+  Ideathon7,
+  Ideathon8,
+  Ideathon9,
+  Ideathon10,
+  Ideathon11,
+  Ideathon12,
+};
