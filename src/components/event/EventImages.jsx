a174aa76b8b4cc9b5225108f162bb1c1f6e2b834
Piyush171104 <141119676+Piyush171104@users.noreply.github.com>
2024-07-21 18:59:10 +0530

    border change

--- src/components/event/EventImages.jsx
+++ src/components/event/EventImages.jsx
@@ -21,26 +21,45 @@ function EventImages() {
     );
   }
 
-  return (
-    <div className="bg-black text-white min-h-screen flex flex-col items-center p-4">
-      <h1 className="text-3xl pb-10">{event.name}</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 px-5">
-        {event.images.map((image, index) => (
+  const renderImageGroup = (group, index) => (
+    <div key={index} className="grid grid-cols-1 sm:grid-cols-3 gap-4 px-5 mb-4">
+      <div className="relative border-4 border-black cursor-pointer col-span-3 sm:col-span-1">
+        <div className="border-4 border-orange-500">
+          <img
+            src={group[0]}
+            alt={`Event Portrait Image ${index * 3 + 1}`}
+            className="block max-w-full max-h-full object-contain"
+          />
+        </div>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 col-span-3 sm:col-span-2 gap-4">
+        {group.slice(1).map((image, idx) => (
           <div
-            className="relative border-4 border-black cursor-pointer flex items-center justify-center"
-            key={index}
+            className="relative border-4 border-black cursor-pointer"
+            key={idx}
           >
-            <div className="border-4 border-orange-500 flex items-center justify-center h-full w-full">
+            <div className="border-4 border-orange-500">
               <img
                 src={image}
-                alt={`Event Image ${index + 1}`}
-                className="max-w-full max-h-full object-contain"
+                alt={`Event Landscape Image ${index * 3 + idx + 2}`}
+                className="block max-w-full max-h-full object-contain"
               />
             </div>
-            <div className="absolute bottom-0 left-0 right-0 bg-orange-500 p-1"></div>
           </div>
         ))}
       </div>
+    </div>
+  );
+
+  const imageGroups = [];
+  for (let i = 0; i < event.images.length; i += 3) {
+    imageGroups.push(event.images.slice(i, i + 3));
+  }
+
+  return (
+    <div className="bg-black text-white min-h-screen flex flex-col items-center p-4">
+      <h1 className="text-3xl pb-10">{event.name}</h1>
+      {imageGroups.map((group, index) => renderImageGroup(group, index))}
     </div>
   );
 }
