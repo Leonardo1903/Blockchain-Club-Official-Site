a79dc9239baedf6d385a32d6e024e1c68fce1ad4
Piyush171104 <141119676+Piyush171104@users.noreply.github.com>
2024-07-23 13:12:53 +0530

    image sizing sorted

---
+++ src/assets/EventAssets/BitManthan/index.js
@@ -0,0 +1,27 @@
+import BitManthan1 from "./BitManthan1.jpeg";
+import BitManthan2 from "./BitManthan2.jpeg";
+import BitManthan3 from "./BitManthan3.jpeg";
+import BitManthan4 from "./BitManthan4.jpeg";
+import BitManthan5 from "./BitManthan5.jpeg";
+import BitManthan6 from "./BitManthan6.jpeg";
+import BitManthan7 from "./BitManthan7.jpeg";
+import BitManthan8 from "./BitManthan8.jpeg";
+import BitManthan9 from "./BitManthan9.jpeg";
+import BitManthan10 from "./BitManthan10.jpeg";
+import BitManthan11 from "./BitManthan11.jpeg";
+import BitManthan12 from "./BitManthan12.jpeg";
+
+export {
+  BitManthan1,
+  BitManthan2,
+  BitManthan3,
+  BitManthan4,
+  BitManthan5,
+  BitManthan6,
+  BitManthan7,
+  BitManthan8,
+  BitManthan9,
+  BitManthan10,
+  BitManthan11,
+  BitManthan12,
+};
