c93d8e323f4d39248e333fa05c8b3c8845d0540f
Piyush171104 <141119676+Piyush171104@users.noreply.github.com>
2024-10-26 20:08:48 +0530

    new event images added

---
+++ src/assets/EventAssets/BlockchainCareers/index.js
@@ -0,0 +1,86 @@
+import BlockchainCareers1 from "./BlockchainCareers1.jpeg";
+import BlockchainCareers2 from "./BlockchainCareers2.jpeg";
+import BlockchainCareers3 from "./BlockchainCareers3.jpeg";
+import BlockchainCareers4 from "./BlockchainCareers4.jpeg";
+import BlockchainCareers5 from "./BlockchainCareers5.jpeg";
+import BlockchainCareers6 from "./BlockchainCareers6.jpeg";
+import BlockchainCareers7 from "./BlockchainCareers7.jpeg";
+import BlockchainCareers8 from "./BlockchainCareers8.jpeg";
+import BlockchainCareers9 from "./BlockchainCareers9.jpeg";
+import BlockchainCareers10 from "./BlockchainCareers10.jpeg";
+import BlockchainCareers11 from "./BlockchainCareers11.jpeg";
+import BlockchainCareers12 from "./BlockchainCareers12.jpeg";
+import BlockchainCareers13 from "./BlockchainCareers13.jpeg";
+import BlockchainCareers14 from "./BlockchainCareers14.jpeg";
+import BlockchainCareers15 from "./BlockchainCareers15.jpeg";
+import BlockchainCareers16 from "./BlockchainCareers16.jpeg";
+import BlockchainCareers17 from "./BlockchainCareers17.jpeg";
+import BlockchainCareers18 from "./BlockchainCareers18.jpeg";
+import BlockchainCareers19 from "./BlockchainCareers19.jpeg";
+import BlockchainCareers20 from "./BlockchainCareers20.jpeg";
+import BlockchainCareers21 from "./BlockchainCareers21.jpeg";
+import BlockchainCareers22 from "./BlockchainCareers22.jpeg";
+import BlockchainCareers23 from "./BlockchainCareers23.jpeg";
+import BlockchainCareers24 from "./BlockchainCareers24.jpeg";
+import BlockchainCareers25 from "./BlockchainCareers25.jpeg";
+import BlockchainCareers26 from "./BlockchainCareers26.jpeg";
+import BlockchainCareers27 from "./BlockchainCareers27.jpeg";
+import BlockchainCareers28 from "./BlockchainCareers28.jpeg";
+import BlockchainCareers29 from "./BlockchainCareers29.jpeg";
+import BlockchainCareers30 from "./BlockchainCareers30.jpeg";
+import BlockchainCareers31 from "./BlockchainCareers31.jpeg";
+import BlockchainCareers32 from "./BlockchainCareers32.jpeg";
+import BlockchainCareers33 from "./BlockchainCareers33.jpeg";
+import BlockchainCareers34 from "./BlockchainCareers34.jpeg";
+import BlockchainCareers35 from "./BlockchainCareers35.jpeg";
+import BlockchainCareers36 from "./BlockchainCareers36.jpeg";
+import BlockchainCareers37 from "./BlockchainCareers37.jpeg";
+import BlockchainCareers38 from "./BlockchainCareers38.jpeg";
+
+export {
+    BlockchainCareers1,
+    BlockchainCareers2,
+    BlockchainCareers3,
+    BlockchainCareers4,
+    BlockchainCareers5,
+    BlockchainCareers6,
+    BlockchainCareers7,
+    BlockchainCareers8,
+    BlockchainCareers9,
+    BlockchainCareers10,
+    BlockchainCareers11,
+    BlockchainCareers12,
+    BlockchainCareers13,
+    BlockchainCareers14,
+    BlockchainCareers15,
+    BlockchainCareers16,
+    BlockchainCareers17,
+    BlockchainCareers18,
+    BlockchainCareers19,
+    BlockchainCareers20,
+    BlockchainCareers21,
+    BlockchainCareers22,
+    BlockchainCareers23,
+    BlockchainCareers24,
+    BlockchainCareers25,
+    BlockchainCareers26,
+    BlockchainCareers27,
+    BlockchainCareers28,
+    BlockchainCareers29,
+    BlockchainCareers30,
+    BlockchainCareers31,
+    BlockchainCareers32,
+    BlockchainCareers33,
+    BlockchainCareers34,
+    BlockchainCareers35,
+    BlockchainCareers36,
+    BlockchainCareers37,
+    BlockchainCareers38,
+};
+
+
+
+
+
+
+
